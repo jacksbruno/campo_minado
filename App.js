28612c5d3c36cdc2206405bdf9a4fc55782c015f
Jacks Bruno Moran Prado <jacks.prado@grupopereira.local>
2022-03-16 10:46:12 -0400

    Implementando o componente Campo

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import params from './src/params'
+import Field from './src/components/Field'
 
   export default (props) => {
     return (
@@ -8,6 +9,13 @@ import params from './src/params'
         <View>
           <Text>Iniciando o campo minado</Text>
           <Text>Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+
+          <Field />
+          <Field opened />
+          <Field opened nearMines={1} />
+          <Field opened nearMines={2} />
+          <Field opened nearMines={3} />
+          <Field opened nearMines={6} />
         </View>
       </SafeAreaView>
     )
